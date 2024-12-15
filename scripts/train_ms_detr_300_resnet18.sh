193dabf89c2f70084f6de35b05716c41b58f56a6
mechaminu <minnov13@khu.ac.kr>
2024-12-15 07:14:43 +0000

    update resnet18 training script

--- scripts/train_ms_detr_300_resnet18.sh
+++ scripts/train_ms_detr_300_resnet18.sh
@@ -1,22 +1,21 @@
 coco_path=/workspace/coco2017
-num_gpus=3
-
 EXP_DIR=/workspace/ms-detr-checkpoints/ms_detr_300_resnet18
+num_gpus=3
 
 mkdir -p $EXP_DIR
 
 GPUS_PER_NODE=$num_gpus ./tools/run_dist_launch.sh $num_gpus python -u main.py \
-   --output_dir /workspace/ms-detr-checkpoints/ms_detr_300_resnet18 \
+   --output_dir $EXP_DIR \
    --with_box_refine \
    --two_stage \
    --dim_feedforward 2048 \
    --epochs 12 \
    --lr_drop 11 \
-   --coco_path=/workspace/coco2017 \
+   --coco_path=$coco_path \
    --num_queries 300 \
    --use_ms_detr \
    --use_aux_ffn \
    --topk_eval 100 \
    --backbone resnet18 \
-   --resume /workspace/ms-detr-checkpoints/ms_detr_300_resnet18/checkpoint.pth
+   --resume $EXP_DIR/checkpoint.pth
    > $EXP_DIR/train.log
